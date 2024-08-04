6fbdc6642b0ccffed1e249cd38e7524715e2772a
Réna Hajjar <renashajjar@gmail.com>
2024-08-03 21:16:38 -0400

    update colour scheme

---
+++ src/app/Pages/Main/Contact.tsx
@@ -0,0 +1,16 @@
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <>
+      <div className="contact-container">
+        <div className="contact-overlay">
+          <div className="contact-title">Contact Me</div>
+          <div className="contact-item">Email: </div>
+          <div className="contact-item">Github: </div>
+          <div className="contact-item">Linkedin: </div>
+        </div>
+      </div>
+    </>
+  );
+}
