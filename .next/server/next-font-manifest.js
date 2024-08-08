38601f3456ae913d60848bb7ea4760a2c8650df7
Réna Hajjar <renashajjar@gmail.com>
2024-08-08 14:58:26 -0400

    ignore .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/renahajjar/portfolio/portfolio/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
-=======
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/renahajjar/portfolio/portfolio/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
->>>>>>> main
