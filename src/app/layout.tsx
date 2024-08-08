d66bdb5f6135bfca453f4977532a9394e0028029
Réna Hajjar <renashajjar@gmail.com>
2024-08-08 18:20:58 -0400

    fix meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
           <link rel="icon" href="/../../public/favicon.ico" />
         </Head>
         <body className={inter.className}>{children}</body>
