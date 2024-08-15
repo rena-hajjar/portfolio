e0ec8b38a8f86fbc5555ad5c14662a25aece238f
Réna Hajjar <renashajjar@gmail.com>
2024-08-15 16:41:22 -0400

    fix timeline code structure

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -11,6 +11,55 @@ import readwise from "./../data/readwise-color.png";
 import queens from "./../data/queens-color.png";
 import perk from "./../data/perk.png";
 
+const timeline = [
+  {
+    id: 1,
+    title: "Backend Software & Data Engineer Intern - OPS",
+    points: [
+      "Building API endpoints in a Spring Boot application and testing with Postman for user email alerts on database changes",
+      "Interacted with above API in an Angular framework to optimize user experience and modernize 20 year old website",
+      "Created 10 automations for reservation booking with Microsoft Power Automate",
+      "Modernized data flow for clients with Azure Data Factory pipelines",
+      "Excel product management skills through Project Management Institue meetings",
+      "Work first hand in an Agile development environment for efficient client satisfaction",
+    ],
+    logo: ontario,
+    alt: "Ontario Logo",
+    date: "May 2024 - September 2024"
+  },
+  {
+    id: 2,
+    title: "Undergraduate ML Research Student - Queen's Perk Lab",
+    points: [
+      "Automate data transfer to match requirements for 3D Slicer modules",
+      "Annotate and prepare surgical video data for Tensorflow training",
+      "Become familiarized with biomedical research process and feed my curiosity"
+    ],
+    logo: perk,
+    alt: "Percutaneous Surgery Lab Logo",
+    date: "October 2023 - Present"
+  },
+  {
+    id: 3,
+    title: "Software Engineer Intern - Readwise",
+    points: [
+      "Worked with React in a beautiful small startup environment",
+      "Learned git version control, Docker and React best principles"
+    ],
+    logo: readwise,
+    alt: "Readwise Logo",
+    date: "May 2023 - September 2023"
+  },
+  {
+    id: 4,
+    title: "Bachelor of Computing with Honours - Queen's University",
+    points: [],
+    logo: queens,
+    alt: "Queen's University Crest",
+    date: "September 2022 - April 2026"
+  }
+];
+
 export default function Timeline() {
   return (
     <>
@@ -21,155 +70,38 @@ export default function Timeline() {
             className="main-timeline"
             animate
           >
-            <VerticalTimelineElement
-              visible={true}
-              className="vertical-timeline-element--work"
-              date="May 2024"
-              icon={
-                <div className="flex justify-center items-center w-full h-full">
-                  <Image
-                    src={ontario}
-                    alt={"Ontario Logo"}
-                    style={{
-                      width: 40,
-                      height: 40,
-                      objectFit: "contain",
-                      borderRadius: "15px",
-                    }}
-                  />
+            {timeline.map((card) => (
+              <VerticalTimelineElement
+                visible={true}
+                className="vertical-timeline-element--work"
+                date={card.date}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <Image
+                      src={card.logo}
+                      alt={card.alt}
+                      style={{
+                        width: 40,
+                        height: 40,
+                        objectFit: "contain",
+                        borderRadius: "15px",
+                      }}
+                    />
+                  </div>
+                }
+              >
+                <div className="vertical-timeline-element-title">
+                  {card.title}
                 </div>
-              }
-            >
-              <div className="vertical-timeline-element-title">
-                Backend Developer Intern - MPBSDP
-              </div>
-              <div className="timeline-element-about">
-                <ul>
-                  <li>
-                    Building API endpoints in a Spring Boot application and
-                    testing with Postman for user email alerts on database
-                    changes
-                  </li>
-                  <li>
-                    Interacted with above API in an Angular framework to
-                    optimize user experience and modernize 20 year old website
-                  </li>
-                  <li>
-                    Created 10 automations for reservation booking with
-                    Microsoft Power Automate
-                  </li>
-                  <li>
-                    Modernized data flow for clients with Azure Data Factory
-                    pipelines
-                  </li>
-                  <li>
-                    Excel product management skills through Project Management
-                    Institue meetings
-                  </li>
-                  <li>
-                    Work first hand in an Agile development environment for
-                    efficient client satisfaction
-                  </li>
-                </ul>
-              </div>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              visible={true}
-              className="vertical-timeline-element--work"
-              date="October 2023"
-              icon={
-                <div className="flex justify-center items-center w-full h-full">
-                  <Image
-                    src={perk}
-                    alt={"Perk lab Logo"}
-                    style={{
-                      width: 40,
-                      height: 40,
-                      objectFit: "contain",
-                      borderRadius: "15px",
-                    }}
-                  />
+                <div className="timeline-element-about">
+                  <ul>
+                    {card.points.map((item) => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
                 </div>
-              }
-            >
-              <div className="vertical-timeline-element-title">
-                Undergraduate ML Research Student - Queen's Perk Lab
-              </div>
-              <div className="timeline-element-about">
-                <ul>
-                  <li>
-                    Automate data transfer to match requirements for 3D Slicer
-                    modules
-                  </li>
-                  <li>
-                    Annotate and prepare surgical video data for Tensorflow
-                    training
-                  </li>
-                  <li>
-                    Become familiarized with biomedical research process and
-                    feed my curiosity
-                  </li>
-                </ul>
-              </div>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              visible={true}
-              className="vertical-timeline-element--work"
-              date="May 2023"
-              icon={
-                <Image
-                  src={readwise}
-                  alt={"Readwise Logo"}
-                  style={{
-                    width: 40,
-                    height: 40,
-                    objectFit: "contain",
-                    borderRadius: "15px",
-                  }}
-                />
-              }
-            >
-              <div className="vertical-timeline-element-title">
-                Software Engineer Intern - Readwise
-              </div>
-              <div className="timeline-element-about">
-                <ul>
-                  <li>
-                    Worked with React in a beautiful small startup environment
-                  </li>
-                  <li>
-                    Learned git version control, Docker and React best
-                    principles
-                  </li>
-                </ul>
-              </div>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              visible={true}
-              className="vertical-timeline-element--work"
-              date="September 2022 - April 2026"
-              icon={
-                <Image
-                  src={queens}
-                  alt={"Queen's University Logo"}
-                  style={{
-                    width: 42,
-                    height: 42,
-                    objectFit: "contain",
-                    borderRadius: "20px",
-                  }}
-                />
-              }
-            >
-              <div className="vertical-timeline-element-title">
-                Bachelor of Computing with Honours - Queen's University
-              </div>
-              <div className="timeline-element-about">
-                <ul>
-                  <li>Want to see more? button to go to coursework page</li>
-                </ul>
-              </div>
-            </VerticalTimelineElement>
+              </VerticalTimelineElement>
+            ))}
           </VerticalTimeline>
         </div>
       </div>
